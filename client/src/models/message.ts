174871291d544d9c4e940e448ce70a336b7283fd
YSHgroup <david.dev100@outlook.com>
2024-08-22 14:54:34 -0400

    Add and update the model

--- client/src/models/message.ts
+++ client/src/models/message.ts
@@ -1,9 +1,9 @@
 export interface Message {
-  id: string,
-  sender_email: string,
-  receiver_email: string,
-  receiver_name: string,
-  sender_name: string,
-  text: string,
+  id: string
+  sender_email: string
+  receiver_email: string
+  receiver_name: string
+  sender_name: string
+  text: string
   createdAt: number
 }
