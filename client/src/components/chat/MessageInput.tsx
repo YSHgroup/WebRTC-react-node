9907f59fa474f9a2707a085acc4ce1841c4f2040
YSHgroup <david.dev100@outlook.com>
2024-08-22 10:38:34 -0400

    Add message sending functionality

--- client/src/components/chat/MessageInput.tsx
+++ client/src/components/chat/MessageInput.tsx
@@ -1,12 +1,48 @@
+import { AuthContext } from '@/context/AuthProvider'
+import { sendPublicMessage } from '@/services/chatting'
+import { KeyboardEventHandler, useContext, useState } from 'react'
 
-const MessageInput = () => {
+const MessageInput = ({type}: {type: 'group' | 'personal'}) => {
+  const [text, setText] = useState<string>('')
+  const { currentUser } = useContext(AuthContext)
+
+  const sendMessage = () => {
+    if (text.trim() === '') return
+    if (type == 'group') {
+      sendPublicMessage({
+        text: text.trim(),
+        sender_email: currentUser!.email as string,
+        sender_name: currentUser!.displayName ?? '',
+      })
+    }
+    setText('')
+  }
+  const enterDown: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
+    if (e.key == 'Enter' && !e.shiftKey) {
+      sendMessage()
+    }
+    return
+  }
+
   return (
-    <div className="flex">
-      <label className="text-start flex w-full">
-        <span className="text-gray-600 leading-7">Message: </span>
-        <input type="text" className="w-full rounded border border-sky-400 p-1 focus:outline-none mx-2 focus:shadow" placeholder="Type a message..." />
+    <div className='flex'>
+      <label className='text-start flex w-full'>
+        <span className='text-gray-600 leading-7'>Message: </span>
+        <textarea
+          rows={1}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={enterDown}
+          className='w-full rounded border border-sky-400 p-1 focus:outline-none mx-2 focus:shadow'
+          placeholder='Type a message...'
+        />
       </label>
-      <button className="text-sky-700 hover:drop-shadow hover:underline">Send</button>
+      <button
+        onClick={sendMessage}
+        className='text-sky-700 hover:drop-shadow hover:underline'
+      >
+        Send
+      </button>
     </div>
   )
 }
